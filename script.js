00e0bc0884410ac44359183b7099f77214024ca0
veera <veeraveerendra11@gmail.com>
2025-04-30 16:52:57 +0530

    Added model functionality for Devops tools

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function to open the modal
+function openModal(tool) {
+  document.getElementById(tool + 'Modal').style.display = 'block';
+}
+
+// Function to close the modal
+function closeModal(tool) {
+  document.getElementById(tool + 'Modal').style.display = 'none';
+}
